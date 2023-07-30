197d969b49306400023c20b26def4ab07acb6072
Lokeshwar Kumar Tabjula <lokeshwartabjula@gmail.com>
2023-07-30 06:23:42 -0300

    adding initial files

---
+++ src/services/todoService.js
@@ -0,0 +1,25 @@
+import api from "./api";
+
+export const getTodos = (email) => {
+  return api.post("todo/home", { email });
+};
+
+export const getTodoById = (id) => {
+  return api.post("todo/details", { id });
+};
+
+export const deleteTodo = (id) => {
+  return api.delete("todo/delete", { data: { id } });
+};
+
+export const CreateNewTodo = (todoDetails) => {
+  return api.post("todo/create", todoDetails);
+};
+
+export const updateTodo = (todoDetails) => {
+  return api.put("todo/update", todoDetails);
+};
+
+export const assignTodo = (todoId, friendEmail) => {
+  return api.post("todo/assign", { todoId, friendEmail });
+};
